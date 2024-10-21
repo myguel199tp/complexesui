2cc862b95ec2ceadedb177163d45972e3c7f99bf
myguel199tp <jmtiradop19@gmail.com>
2024-10-20 20:20:37 -0500

    feat: se remueve comentario

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
 import "./index.css";
 export {Button} from './Button/Button'
 export {Title} from './Title/Title'
-
-//https://www.youtube.com/watch?v=nno-ZtHM6wg&list=PLoOnCUvhzJYOk_s-NQOeWdzPaYmXZGzDj&index=4
